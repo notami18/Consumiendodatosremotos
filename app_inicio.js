1cfc3306d1a50abf0a32c2bdecac2e35ad82c2cc
Andrés notami <andresnotami@gmail.com>
2017-02-16 17:26:25 -0500

    Cambios
    Se cambian las versiones del angular.

--- app_inicio.js
+++ app_inicio.js
@@ -19,11 +19,11 @@ angular.module('myApp', ['ngAnimate'])
         }
         
         $scope.obtenerClima = function() {
-            var url = 'https://query.yahooapis.com/v1/public/yql?q=select%20*%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22nome%2C%20ak%22)&format=json&env=store%3A%2F%2Fdatatables.org%2Falltableswithkeys';
+            var url = 'http://aspirantes.hablacreativo.com/api/v1/admission_test';
             $http.get(url)
             .then(function(res){
-                $scope.clima = res.data.query.results.channel.item.condition
-                console.log(res.data);
+                $scope.info = res.data.data["table-data"].values
+                console.log(res.data.data["table-data"].values);
             })
         }
     })
